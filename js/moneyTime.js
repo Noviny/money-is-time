902b794ed661917dea28fd86aa58c2577401972d
Noviny BC <benjaminconolly@gmail.com>
2016-01-09 13:07:18 +1100

    added fortnightly option for the lazy, and neatened the code

--- js/moneyTime.js
+++ js/moneyTime.js
@@ -1,17 +1,40 @@
 var hourlyRateGlobal = 0;
+var timeSpent = 0
 
+
+var getWorth = function (worth) {
+  if ( worth < 1 ) {
+      var minutes = Math.round( worth*60 );
+      return (minutes + " minutes")
+    } else if ( 24 >= worth >= 1 ) {
+      var hours = Math.floor(worth);
+      var minutes = Math.round( ( worth%1 )*60 );
+      return (hours + " hours, and " + minutes + " minutes")
+    } else if ( worth >= 24 ) {
+      var days = Math.floor( worth/24 )
+      var hours = Math.floor( worth%24 );
+      var minutes = Math.round( ( worth%1 )*60 );
+      return (days + " days," + hours + " hours, and " + minutes + " minutes" )
+    }
+}
 
 $( document ).ready ( function () {
+
   $('button.hourly-rate').click( function () {
     event.preventDefault();
     var results = $('form.hourly-rate').serializeArray()
     var hourlyRate = 0;
-    if ( results[2].value === "hourly" ) {
-      hourlyRate = results[1].value;
-    } else if  ( results[2].value === "weekly" ) {
-      hourlyRate = results[1].value/results[0].value;
-    } else if  ( results[2].value === "yearly" ) {
-      hourlyRate = (results[1].value/52)/results[0].value;
+    var hours = results[0].value
+    var salary = results[1].value
+    var salaryType = results[2].value
+    if ( salaryType === "hourly" ) {
+      hourlyRate = salary;
+    } else if  ( salaryType === "weekly" ) {
+      hourlyRate = (salary/hours).toFixed(2);
+    } else if (salaryType === "fortnightly") {
+      hourlyRate = ((salary/2)/hours).toFixed(2);
+    }else if  ( salaryType === "yearly" ) {
+      hourlyRate = ((salary/52)/hours).toFixed(2);
     } else {
       window.alert("Please pick your pay type");
     }
@@ -23,13 +46,18 @@ $( document ).ready ( function () {
     event.preventDefault();
     var results = $('form.item-worth-form').serializeArray();
     var worth = results[1].value / hourlyRateGlobal;
-    if ( worth < 1 ) {
-      var minutes = Math.round(worth*60);
-      $( 'div.worth' ).append('<p>' + results[0].value + ' costs: ' + minutes + ' minutes.</p>');
-    } else if ( worth >= 1 ) {
-      var hours = Math.floor(worth);
-      var minutes = Math.round( ( worth%1 )*60);
-      $( 'div.worth' ).append('<p>' + results[0].value + ' costs: ' + hours + ' hours and ' + minutes + ' minutes.</p>');
-    }
+    timeSpent += worth
+    overallWorth = getWorth(timeSpent)
+    localWorth = getWorth(worth)
+    $('#items').append( "<li>" + results[0].value + " is worth " + localWorth + "</li>" )
+    $('#totals').html(overallWorth)
   })
+
+  $( 'button.reset' ).click( function () {
+    timeSpent = 0
+    $('#items').html( "" )
+    $('#totals').html( "" )
+  } )
 });
+
+
